0e0841bfc3fc13b09b796ee99f84ff4d79fecdfa
Kien <vacnike911@gmail.com>
2024-05-28 15:23:50 +0300

    28.05.24 csv

--- 11_csv.py
+++ 11_csv.py
@@ -3,11 +3,13 @@ import pandas as pd
 l_conect = [
     {
         'user_name': 'kien',
-        'passwords': '1234'
+        'passwords': '1234',
+        'khui': '49,5'
     },
     {
         'user_name': 'qwewrt',
-        'passwords': '9876'
+        'passwords': '9876',
+        'khui': '4'
     }
 ]
 
